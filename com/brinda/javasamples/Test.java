d1e42059392c94b6e7f57098591775b96eb95b5d
SivakumarParthiban <pvpsiva@gmail.com>
2023-08-23 12:33:08 -0400

    this keyword
    Using ‘this’ keyword to refer to current class instance variables.

---
+++ com/brinda/javasamples/Test.java
@@ -0,0 +1,19 @@
+package com.brinda.javasamples;
+
+public class Test{
+	
+	int a;int b;
+	
+	public Test(int a,int b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	
+	public void display()
+	{
+		System.out.println(" a = " + a + " b = " + b);
+		
+	}
+
+}
---
+++ thiskeywordtorefertocurrentclassinstancevariables/ThisKeyword.java
@@ -0,0 +1,14 @@
+package thiskeywordtorefertocurrentclassinstancevariables;
+
+import com.brinda.javasamples.Test;
+
+public class ThisKeyword {
+
+	public static void main(String[] args) {
+		
+		Test obj = new Test(10,20);
+		obj.display();
+
+	}
+
+}
